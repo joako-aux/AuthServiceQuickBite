eeba36b958449d41357c90a660f9dbec690d2411
joako perez <8bussy8@gmail.com>
2026-06-23 09:02:31 -0400

    04

--- AuthServiceQuickBite/src/main/java/com/example/AuthServiceQuickBite/config/SecurityConfig.java
+++ AuthServiceQuickBite/src/main/java/com/example/AuthServiceQuickBite/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,6 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
 
 public class SecurityConfig {
@@ -26,7 +28,8 @@ public class SecurityConfig {
         this.jwtFilter = jwtFilter;
     }
 
-    @Bean
+    @Bean(name = "defaultSecurityFilterChain")
+
     SecurityFilterChain securityFilterChain(
             HttpSecurity http
     ) throws Exception {
